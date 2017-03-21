b8fe0024ed08b7fdc35303388bdbac48d29d1bd9
adler3d <adler3d@gmail.com>
2017-03-21 13:51:52 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -4,16 +4,9 @@ static real get_pi2(int iters=16)
   auto a=vec2d(1,0);
   auto b=vec2d(0,1);
   auto n=4;
-  auto sqrt=[](vec2d c){return c.Mag();};
-  auto norm=[](vec2d c){return c.Norm();};
-  auto f=[&](vec2d a,vec2d b){
-    auto c=(a+b)*0.5;
-    c=norm(c);
-    return c;
-  };
   for(int i=0;i<iters;i++){
-    b=f(a,b);
+    b=(a-b).Norm();
     n<<=1;
   }
-  return sqrt(b-a)*n;
+  return (b-a).Mag()*n;
 }
