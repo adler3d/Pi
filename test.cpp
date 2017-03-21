55d0853489318c0aad6845bd9a4886687f2435dc
adler3d <adler3d@gmail.com>
2017-03-21 13:52:56 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,10 +1,10 @@
 //qap_text_draw_at_x_center(qDev,vec2d(0,200),"pi = "+FToS(get_pi2()*0.5e6),16);
-static real get_pi2(int iters=16)
+static real get_pi2(int k=16)
 {
   auto a=vec2d(1,0);
   auto b=vec2d(0,1);
   auto n=4;
-  for(int i=0;i<iters;i++){
+  for(int i=0;i<k;i++){
     b=(a+b).Norm();
     n<<=1;
   }
